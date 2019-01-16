d8ece8a70ecfee19fc43e25f8d430a9d6e8d1056
Keshav Sai Deekonda <keshavsaidee@usf.edu>
2019-01-15 19:19:51 -0500

    name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Keshav";
+            Console.WriteLine("hello name");
+            Console.ReadLine("hello name")
         }
     }
 }
